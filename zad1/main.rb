38998a314167f4dd8bfdd4eecdabb1dbda769267
Arkadiusz Ryk <arekr97@gmail.com>
2018-11-01 22:09:40 +0100

    test;

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,21 +1,24 @@
 def sort_letters(string)
-
+    string = string.chars.sort.join
 end
 
 def count_vowels(string)
-
+    string.downcase!
+    a = string.count("aeiouy")   
 end
 
 def filter_odd(array)
-
+    array = array.select{ |n| n.odd? }
 end
 
-sort_letters('dcba') #=> 'abcd'
-sort_letters('zyx') #=> 'xyz'
+puts sort_letters('dcba') #=> 'abcd'
+puts sort_letters('zyx') #=> 'xyz'
 
-count_vowels('mmmm') #=> 0
-count_vowels('super') #=> 2
-count_vowels('super extra') #=> 4
+puts count_vowels('mmmm') #=> 0
+puts count_vowels('super') #=> 2
+puts count_vowels('super extra') #=> 4
 
-filter_odd([1, 2, 3, 4, 5]) #=> [1, 3, 5]
-filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
+puts filter_odd([1, 2, 3, 4, 5]).join(" , ") #=> [1, 3, 5]
+puts filter_odd([6, 7, 9, 2, 6, 5]).join(" , ") #=> [7, 9, 5]
+
+#Arkadisz Ryk 01.11.2018
