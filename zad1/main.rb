006823d0b1661545e9aca42ade5a78cf95d0748a
OfftheCode <offthecode@gmail.com>
2018-11-01 21:55:51 +0100

    implemented tasks specified in readme for zad1

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,21 +1,21 @@
 def sort_letters(string)
-
+  string.delete(" ").chars.sort.join # pass to sort if char case matters (&:casecmp)
 end
 
 def count_vowels(string)
-
+  string.count('aeiouy')
 end
 
 def filter_odd(array)
-
+  array.select { |num| num.odd? }
 end
 
-sort_letters('dcba') #=> 'abcd'
-sort_letters('zyx') #=> 'xyz'
+puts sort_letters('dcba') #=> 'abcd'
+puts sort_letters('zyx') #=> 'xyz'
 
-count_vowels('mmmm') #=> 0
-count_vowels('super') #=> 2
-count_vowels('super extra') #=> 4
+puts count_vowels('mmmm') #=> 0
+puts count_vowels('super') #=> 2
+puts count_vowels('super extra') #=> 4
 
-filter_odd([1, 2, 3, 4, 5]) #=> [1, 3, 5]
-filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
+print nums = filter_odd([1, 2, 3, 4, 5]), "\n" #=> [1, 3, 5]
+print nums2 = filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
