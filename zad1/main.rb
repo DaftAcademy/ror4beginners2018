ade8bb97be843fb231f34470d768048e59fbea7d
OfftheCode <offthecode@gmail.com>
2018-11-01 22:10:28 +0100

    Completed zad1

--- zad1/main.rb
+++ zad1/main.rb
@@ -7,15 +7,15 @@ def count_vowels(string)
 end
 
 def filter_odd(array)
-  array.select { |num| num.odd? }
+  array.select(&:odd?)
 end
 
-puts sort_letters('dcba') #=> 'abcd'
-puts sort_letters('zyx') #=> 'xyz'
+sort_letters('dcba') #=> 'abcd'
+sort_letters('zyx') #=> 'xyz'
 
-puts count_vowels('mmmm') #=> 0
-puts count_vowels('super') #=> 2
-puts count_vowels('super extra') #=> 4
+count_vowels('mmmm') #=> 0
+count_vowels('super') #=> 2
+count_vowels('super extra') #=> 4
 
-print nums = filter_odd([1, 2, 3, 4, 5]), "\n" #=> [1, 3, 5]
-print nums2 = filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
+filter_odd([1, 2, 3, 4, 5]) #=> [1, 3, 5]
+filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
