d9cb16e92fa7589017a56ee151d8c378213df033
Iga <iga.supinska@gmail.com>
2018-11-02 20:18:37 +0100

    solutions for first task

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,13 +1,26 @@
 def sort_letters(string)
-
+	puts string.chars.sort.join
 end
 
 def count_vowels(string)
-
+	vowels = Array['a', 'e', 'i', 'o', 'u', 'y']
+	count = 0
+	string.each_char { |letter|
+		if vowels.include?(letter)
+			count += 1
+		end
+	}
+	puts count
 end
 
 def filter_odd(array)
-
+	oddNumbers = Array.new
+	for number in array
+		if number % 2 != 0
+			oddNumbers.push(number)
+		end
+	end
+	puts "#{oddNumbers}"
 end
 
 sort_letters('dcba') #=> 'abcd'
