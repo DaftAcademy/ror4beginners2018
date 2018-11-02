501af654802fda9b87b34063ffcc2bd15e5324ba
Maxization <damian-opoka@wp.pl>
2018-11-02 19:04:18 +0100

    Update main.rb

--- zad1/main.rb
+++ zad1/main.rb
@@ -35,14 +35,3 @@ def filter_odd(array)
     end
     return array
 end
-
-
-sort_letters('dcba') #=> 'abcd'
-sort_letters('zyx') #=> 'xyz'
-
-count_vowels('mmmm') #=> 0
-count_vowels('super') #=> 2
-count_vowels('super extra') #=> 4
-
-filter_odd([1, 2, 3, 4, 5]) #=> [1, 3, 5]
-filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
