b09304ba97a77222d032deaa890e1559ee026a16
Maxization <damian.opoka99@gmail.com>
2018-11-02 18:47:23 +0100

    Zadanie1

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,14 +1,41 @@
 def sort_letters(string)
-
+    i=0
+    while i<string.length
+        j=i
+        while j<string.length
+            if string[i]>string[j]
+                w=string[j]
+                string[j]=string[i]
+		string[i]=w
+            end
+            j+=1
+        end
+        i+=1
+    end
+    return string
 end
 
 def count_vowels(string)
-
+    x=0
+    i=0
+    while i<string.length 
+        if string[i]=='a' || string[i]=='e' || string[i]=='i' || string[i]=='o' || string[i]=='u' || string[i]=='y'
+        x+=1
+        end
+    i+=1
+    end
+    return x
 end
 
 def filter_odd(array)
-
+    for i in array
+        if i%2 == 0
+            array.delete(i)
+        end
+    end
+    return array
 end
+
 
 sort_letters('dcba') #=> 'abcd'
 sort_letters('zyx') #=> 'xyz'
