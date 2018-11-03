9d0c3ffa32eadc7a39742e887203ae50bf7e1692
balkon16 <pawel.k.lonca@gmail.com>
2018-11-03 15:30:20 +0100

    Pierwsza praca domowa zrobiona

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,13 +1,28 @@
 def sort_letters(string)
-
+    return string.split("").sort().join
 end
 
 def count_vowels(string)
-
+    vowels = 'eyuioa'
+    odpowiedz = 0
+    string.split("").each {
+        |litera|
+        if vowels.include?(litera)
+            odpowiedz = odpowiedz + 1
+        end
+    }  
+    return odpowiedz
 end
 
 def filter_odd(array)
-
+    odpowiedz = Array.new
+    array.each {
+        |liczba|
+        if liczba % 2 == 1
+            odpowiedz.push(liczba)
+        end
+    }
+    return odpowiedz
 end
 
 sort_letters('dcba') #=> 'abcd'
