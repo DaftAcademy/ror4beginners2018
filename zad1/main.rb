0a9d56c0d92ecb10ec64a4ef68957107952cb282
Mateusz Zieba <mateusz.zieba@onet.pl>
2018-11-03 17:11:26 +0100

    zad1 solution

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,21 +1,15 @@
 def sort_letters(string)
-
+	string.gsub(/\s+/, '').split('').sort.join
 end
 
 def count_vowels(string)
-
+	def isVowel s
+		'aeiouy'.each_char {|a| return true if a.eql?(s)}
+	false
+end
+	string.each_char.select {|a| isVowel(a)}.length
 end
 
 def filter_odd(array)
-
+	array.select(&:odd?)
 end
-
-sort_letters('dcba') #=> 'abcd'
-sort_letters('zyx') #=> 'xyz'
-
-count_vowels('mmmm') #=> 0
-count_vowels('super') #=> 2
-count_vowels('super extra') #=> 4
-
-filter_odd([1, 2, 3, 4, 5]) #=> [1, 3, 5]
-filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
