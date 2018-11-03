c5d2915f8fc4b5b4189291f394fe5b33b1515572
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2018-11-03 21:27:29 +0100

    zad1 added

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,13 +1,13 @@
 def sort_letters(string)
-
+  string.chars.sort.join
 end
 
 def count_vowels(string)
-
+  string.downcase.count('eyuioaęóą')
 end
 
 def filter_odd(array)
-
+  array.select { |n| n % 2 == 1 }
 end
 
 sort_letters('dcba') #=> 'abcd'
