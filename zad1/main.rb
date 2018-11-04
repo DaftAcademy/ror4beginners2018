a81c2110da726bfcdff926b6da2919362cd6d890
janryb96 <jan.rybicki96@wp.pl>
2018-11-04 11:38:24 +0100

    main.rb

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,21 +1,21 @@
 def sort_letters(string)
-
+    string.chars.sort_by(&:downcase).join
 end
 
 def count_vowels(string)
-
+    string.scan(/[aeiouy]/).count
 end
 
 def filter_odd(array)
-
+    array.select(&:odd?).join
 end
 
-sort_letters('dcba') #=> 'abcd'
-sort_letters('zyx') #=> 'xyz'
+puts sort_letters('dcba') #=> 'abcd'
+puts sort_letters('zyx') #=> 'xyz'
 
-count_vowels('mmmm') #=> 0
-count_vowels('super') #=> 2
-count_vowels('super extra') #=> 4
+puts count_vowels('mmmm') #=> 0
+puts count_vowels('super') #=> 2
+puts count_vowels('super extra') #=> 4
 
-filter_odd([1, 2, 3, 4, 5]) #=> [1, 3, 5]
-filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
+puts filter_odd([1, 2, 3, 4, 5]) #=> [1, 3, 5]
+puts filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
