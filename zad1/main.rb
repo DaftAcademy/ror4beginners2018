7330f2c7d3fa832bb2ca624ebec2e039d40d6396
Tomek Gonciarz <tomas.gonciarz@gmail.com>
2018-11-04 09:32:45 +0100

    First task

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,21 +1,21 @@
 def sort_letters(string)
-
+    string.chars.sort.join
 end
 
 def count_vowels(string)
-
+    string.count('aeoui')
 end
 
 def filter_odd(array)
-
+    array.select(&:odd?)
 end
 
-sort_letters('dcba') #=> 'abcd'
-sort_letters('zyx') #=> 'xyz'
+puts sort_letters('dcba') #=> 'abcd'
+puts sort_letters('zyx') #=> 'xyz'
 
-count_vowels('mmmm') #=> 0
-count_vowels('super') #=> 2
-count_vowels('super extra') #=> 4
+puts count_vowels('mmmm') #=> 0
+puts count_vowels('super') #=> 2
+puts count_vowels('super extra') #=> 4
 
-filter_odd([1, 2, 3, 4, 5]) #=> [1, 3, 5]
-filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
+p(filter_odd([1, 2, 3, 4, 5])) #=> [1, 3, 5]
+p(filter_odd([6, 7, 9, 2, 6, 5])) #=> [7, 9, 5]
