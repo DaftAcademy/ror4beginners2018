8ab8526bb8acd333ef93c5804caf6ff9f66399f4
Monika <monika.chudek1@gmail.com>
2018-11-05 21:24:58 +0100

    ''

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,21 +1,22 @@
-def sort_letters(string)
-
+def sort_letters string
+   string.chars.sort.join
 end
 
-def count_vowels(string)
-
+def count_vowels string
+     string.downcase.scan(/[aeiouy]/).length
 end
 
-def filter_odd(array)
-
+def filter_odd(numbers)
+    numbers.select(&:odd?)
 end
 
-sort_letters('dcba') #=> 'abcd'
-sort_letters('zyx') #=> 'xyz'
+# testy
+puts sort_letters('dcba') == 'abcd'
+puts sort_letters('zyx') == 'xyz'
 
-count_vowels('mmmm') #=> 0
-count_vowels('super') #=> 2
-count_vowels('super extra') #=> 4
+puts count_vowels('mmmm') == 0
+puts count_vowels('sUper') == 2
+puts count_vowels('super extra') == 4
 
-filter_odd([1, 2, 3, 4, 5]) #=> [1, 3, 5]
-filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
+puts filter_odd([1, 2, 3, 4, 5]) == [1, 3, 5]
+puts filter_odd([6, 7, 9, 2, 6, 5]) == [7, 9, 5]
