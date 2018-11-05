bc57ed3f24220eaefc735ac1827423db7915ecc5
Wojciech Szczepański <wszczepanski97@gmail.com>
2018-11-05 19:55:29 +0100

    Zadanie1 zrobione

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,13 +1,13 @@
 def sort_letters(string)
-
+    string.split("").sort().join
 end
 
 def count_vowels(string)
-
+string.count 'aeiou'
 end
 
 def filter_odd(array)
-
+    array.select(&:odd?)
 end
 
 sort_letters('dcba') #=> 'abcd'
