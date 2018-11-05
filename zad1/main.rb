a4b501e049a47f5bb67110037f2b3d7b80bb241a
jmichalak9 <michalakjakub1999@gmail.com>
2018-11-05 21:09:30 +0100

    Update zad1/main.rb

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,13 +1,13 @@
 def sort_letters(string)
-
+    string.chars.sort.join
 end
 
 def count_vowels(string)
-
+    string.count 'aeiouy'
 end
 
 def filter_odd(array)
-
+    array.select &:odd?
 end
 
 sort_letters('dcba') #=> 'abcd'
