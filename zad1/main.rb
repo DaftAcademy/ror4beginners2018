fd34aa5f3c34bd17e6a406b292f5005220be1fb6
grochowskih <44784916+grochowskih@users.noreply.github.com>
2018-11-05 20:50:23 +0100

    Update main.rb
    Rozwiązane (5/11/18)

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,12 +1,18 @@
 def sort_letters(string)
 
+  string.chars.sort.join()
+  
 end
 
 def count_vowels(string)
 
+  string.count('aeiouy')
+  
 end
 
 def filter_odd(array)
+
+  array.select(&:odd?)
 
 end
 
