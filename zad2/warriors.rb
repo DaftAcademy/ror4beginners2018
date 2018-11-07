4a65d0132475b182f51a6cb06fac60b3e1ac423c
OfftheCode <offthecode@gmail.com>
2018-11-08 00:12:58 +0100

    Created Character base class

--- zad2/warriors.rb
+++ zad2/warriors.rb
@@ -1,3 +1,31 @@
+##
+# This class represents basic character in game containing must have values to define single identity
+class Character
+
+  ##
+  # Allows the user to identify the created character during the program runtime
+  attr_accessor :name
+
+  ##
+  # Level defines the character level but is also used as it's base damge value
+  attr_accessor :level
+
+  def initialize(name, level)
+    self.name = name
+    self.level = level
+  end
+
+  ##
+  # Strength is used to calculate character 'power' during the fight it sums character lv and rolls d12
+  def strength
+    level + [*1..12].sample
+  end
+
+end
+
+
+
+
 class Warrior
   def initialize(name:, level:)
   end
