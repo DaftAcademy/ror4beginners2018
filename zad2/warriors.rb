d7b53f68e473b8915ab882fba624d92035a59c30
OfftheCode <offthecode@gmail.com>
2018-11-08 02:17:30 +0100

    Added battle mode and gaining_exp possibility

--- zad2/warriors.rb
+++ zad2/warriors.rb
@@ -52,14 +52,28 @@ class Character
 end
 
 
-
+##
+# This class represents a warrior who can join a BattleArena and gain exp
 class Warrior < Character
   def initialize(name: nil, level: nil)
     super(name: name, level: level)
   end
 
+  ##
+  # Gives expirience to a warrior but only if his enemy is at least of type Character
+  # If enemy is same or lower level warrior gets 1 lv if enemy has higher level he gets enough to eqauls him
+  def gain_exp(rival)
+    if (rival.class <= Character)
+      @level >= rival.level ? @level += 1 : @level += rival.level
+    else
+      puts "Rival needs to be at least a character, come on don't be a coward!"
+    end
+  end
+
 end
 
+##
+# This class represents a monster who can join a BattleArena
 class Monster < Character
   def initialize(name: nil, level: nil)
     super(name: name, level: level)
@@ -68,6 +82,60 @@ class Monster < Character
 end
 
 class BattleArena
+  attr_accessor :first_character, :second_character
   def initialize(first_character, second_character)
+    self.first_character = first_character
+    self.second_character = second_character
   end
+
+  ##
+  # Battle simulates a fight between two characters
+  # Rules:
+  # each character sums his level with dice roll (12d) and function compare it
+  # afterwards grants exp if character was a warrior for more see gain_exp Warrior method 
+  def battle!
+
+    first_name = self.first_character.name
+    first_damage = self.first_character.strength
+    second_name = self.second_character.name
+    second_damage = self.second_character.strength
+
+    puts "#{first_name} attacked #{second_name} with #{first_damage}"
+    puts "#{second_name} attacked #{first_name} with #{second_damage}"
+
+    # TODO: Check if a figher is a warrior as only they can recive exp
+    if first_damage > second_damage
+      puts "#{first_name} won"
+      self.first_character.gain_exp(self.second_character)
+    elsif second_damage > first_damage
+      puts "#{second_name} won"
+      self.second_character.gain_exp(self.first_character)
+    else 
+      # TODO: Implement smt more on a draw?
+      puts "it's a draw!"
+    end
+
+  end
+
 end
+
+# Tests:
+# Hope will work 
+
+# -- preparations
+warrior1 = Warrior.new(name: 'Po', level:  1)
+warrior2 = Warrior.new(name: 'Tai Lung', level: 1)
+monster1 = Monster.new(name: 'Skeleton Mage', level: 15)
+
+arena = BattleArena.new(warrior1, warrior2)
+arena.battle!
+
+puts "-- First round results --"
+puts warrior1.card
+puts warrior2.card
+
+arena = BattleArena.new(warrior1, warrior2)
+arena.battle!
+puts "-- Second round results --"
+puts warrior1.card
+puts warrior2.card
