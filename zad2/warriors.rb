b7e24c932091650ecc6a76cdc1bf9c6807fb3eaa
Jakub Frąc <jakubfrac@MacBook-Pro-Jakub-2.local>
2018-11-11 21:33:56 +0100

    zad2

--- zad2/warriors.rb
+++ zad2/warriors.rb
@@ -1,12 +1,68 @@
-class Warrior
+class Character
+  attr_accessor :name , :level
   def initialize(name:, level:)
+    self.name = name
+    self.level = level
   end
 
   def strength
+    self.level+[*1..12].sample
+  end
+
+  def card
+    "#{name}, level #{level}"
   end
 end
 
-class BattleArena
-  def initialize(first_character, second_character)
+class Warrior < Character
+  def level_up up
+    self.level += up
   end
 end
+
+class Monster < Character
+
+end
+
+class BattleArena
+  attr_accessor :first_character, :second_character
+
+  def initialize(first_character, second_character)
+    self.first_character = first_character
+    self.second_character = second_character
+  end
+
+  def battle!
+    p '-------FIGHT-------'
+    p "#{first_character.card} vs #{second_character.card}"
+    first_strike = first_character.strength
+    second_strike = second_character.strength
+    p ("#{first_character.name} strikes for #{first_strike}")
+    p ("#{second_character.name} strikes for #{second_strike}")
+    if(first_strike == second_strike)
+      p 'Its a tie!'
+      return
+    elsif(first_strike > second_strike)
+      winner = first_character
+      loser = second_character
+    else
+      winner = second_character
+      loser = first_character
+    end
+    if(winner.is_a? Warrior)
+      winner.level_up([1, loser.level-winner.level+1].max)
+    end
+    p "Winner is #{winner.card}"
+  end
+end
+
+warrior1 = Warrior.new(name: 'Po', level:  1)
+warrior2 = Warrior.new(name: 'Tai Lung', level: 1)
+monster1 = Monster.new(name: 'Skeleton Mage', level: 15)
+
+arena = BattleArena.new(warrior1, warrior2)
+arena.battle!
+arena.battle!
+arena.battle!
+arena.battle!
+
