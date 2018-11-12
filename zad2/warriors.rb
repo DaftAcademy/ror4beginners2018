d7d3275dca0c4ff596dc4ace758c215218fe6c59
w-kn <44850728+w-kn@users.noreply.github.com>
2018-11-12 22:38:52 +0100

    Update warriors.rb

--- zad2/warriors.rb
+++ zad2/warriors.rb
@@ -1,12 +1,78 @@
-class Warrior
-  def initialize(name:, level:)
+class Character
+    attr_accessor :name
+    attr_accessor :level
+
+    def initialize(name:, level:)
+        self.name=name
+        self.level=level
+    end
+  
+    def strength
+        level+[*1..12].sample
+    end
+
   end
 
-  def strength
-  end
+
+class Warrior < Character
+    def card
+        "Warrior #{name} - level #{level}"
+    end
 end
-
+  
+class Monster < Character
+    def card
+        "Monster #{name} - level #{level}"
+    end
+end
+  
 class BattleArena
-  def initialize(first_character, second_character)
-  end
+    attr_accessor :first_character, :second_character
+    def initialize(first_character, second_character)
+        @first_character=first_character
+        @second_character=second_character
+    end
+
+    def battle!
+        winner = Character.new(name: 'NN', level: 1)
+        puts "#{first_character.name} attacked #{second_character.name} with #{first_character.strength} damage."
+        puts "#{second_character.name} attacked #{first_character.name} with #{second_character.strength} damage."
+        if second_character.strength < first_character.strength
+            winner = first_character
+        elsif (second_character.strength == first_character.strength && [*1..2]<1.5)
+            winner = first_character
+        else
+            winner = second_character
+        end
+        
+        puts "#{winner.name} won."
+        if winner.is_a?(Warrior)
+            winner.level += 1
+        end
+        
+
+    end
 end
+
+
+
+warrior1 = Warrior.new(name: 'Po', level:  1)
+warrior2 = Warrior.new(name: 'Tai Lung', level: 1)
+monster1 = Monster.new(name: 'Skeleton Mage', level: 15)
+puts warrior1.card # => Po (lvl 1)
+puts warrior1.strength # => 6
+puts warrior1.strength # => 5
+puts warrior2.card # => Tai Lung (lvl 1)
+puts monster1.card # => Skeleton Mage (lvl 15)
+
+arena = BattleArena.new(warrior1, warrior2)
+arena.battle!
+puts warrior1.card
+puts warrior2.card
+
+arena = BattleArena.new(warrior1, warrior2)
+arena.battle!
+puts warrior1.card
+puts warrior2.card
+
+
