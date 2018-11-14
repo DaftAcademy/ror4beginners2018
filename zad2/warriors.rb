cb3a1ce7c917408036604a56cff88842225af451
dmarkiewicz <dariusz.markiewicz4@gmail.com>
2018-11-14 15:49:13 +0100

    Fix

--- zad2/warriors.rb
+++ zad2/warriors.rb
@@ -1,31 +1,40 @@
+class LevelError < StandardError
+  def initialize
+    super('Level must be an integer between 1 and 99')
+  end
+end
+
 class Character
   attr_accessor :name, :level
 
   def initialize(name:, level:)
     @name = name
-    @level = validate_level(level) ? level : 1
+    if validate_level(level)
+      @level = level
+    else
+      raise LevelError
+    end
   end
 
   def card
-    "#{@name} (lvl #{@level})"
+    "#{name} (lvl #{level})"
   end
 
   def strength
-    [*1..12].sample + @level
+    [*1..12].sample + level
   end
 
   private
 
-  def validate_level(level)
-    level.between?(1, 99)
-  rescue
-    false
+  def validate_level(lvl)
+    return false unless lvl.is_a?(Integer) && lvl.between?(1,99)
+    return true
   end
 end
 
 class Warrior < Character
   def level_up(n)
-    @level = @level + n <= 99 ? @level + n : 99
+    @level += n if validate_level(@level + n)
   end
 end
 
@@ -39,19 +48,19 @@ class BattleArena
   end
 
   def battle!
-    first_character_strength = @first_character.strength
-    second_character_strength = @second_character.strength
+    first_character_strength = first_character.strength
+    second_character_strength = second_character.strength
 
-    attack(@first_character, @second_character, first_character_strength)
-    attack(@second_character, @first_character, second_character_strength)
+    attack(first_character, second_character, first_character_strength)
+    attack(second_character, first_character, second_character_strength)
 
     if first_character_strength == second_character_strength
       puts "Draw"
     else
       winner, levels_up = if first_character_strength > second_character_strength
-          [@first_character, count_levels(@first_character, @second_character)]
+          [first_character, count_levels(first_character, second_character)]
         else
-          [@second_character, count_levels(@second_character, @first_character)]
+          [second_character, count_levels(second_character, first_character)]
         end
 
       puts "#{winner.name} won"
@@ -60,6 +69,8 @@ class BattleArena
   end
 
   private
+
+  attr_reader :first_character, :second_character
 
   def attack(attacker, defender, strength)
     puts "#{attacker.name} attacked #{defender.name} with #{strength} damage" 
