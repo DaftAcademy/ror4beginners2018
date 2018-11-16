15bd11eeda368a9886619786419811016ba72c02
ozel981 <ozel981@interia.pl>
2018-11-16 09:28:05 +0100

    zad2 poprawka

--- zad2/zad2.rb
+++ zad2/zad2.rb
@@ -1,12 +1,12 @@
 class Character
 	
 	attr_reader :name, :level
-	
+
 	def initialize(name:, level:)
 		@name = name
 		@level = level
 	end
-	
+
 	 def card
 		return "#{@name}  (lvl #{@level})" 
 	end
@@ -16,9 +16,9 @@ class Character
 end
 
 class Warrior < Character
-  def level_up(enemy_level)
+	def level_up(enemy_level)
 		@level=enemy_level + 1
-  end
+	end
 end
 
 class Monster < Character
@@ -27,31 +27,31 @@ end
 class BattleArena
 	attr_accessor :first_character, :second_character
 	
-  def initialize(first_character, second_character)
+	def initialize(first_character, second_character)
 		self.first_character = first_character
 		self.second_character = second_character
-  end
-  
-  def  battle_score(winner,loser)
-		puts "#{winner.name} won!"		
-		if winner.is_a? Warrior and winner.level<=loser.level
-			winner.level_up(loser.level)
-		end
-  end
-  
-  def battle!
+	end
+
+	def battle!
 		first_character_strength = first_character.strength
 		second_character_strength = second_character.strength
-		
+
 		puts "#{first_character.name} attacked #{second_character.name} with #{first_character_strength} damage"
 		puts "#{second_character.name}  attacked #{first_character.name} with #{second_character_strength} damage"
-		
+
 		if first_character_strength > second_character_strength
 			battle_score(first_character,second_character)
 		elsif first_character_strength < second_character_strength
 			battle_score(second_character,first_character)
 		else
 			puts "Draw!"
-    end
-  end
+	end
+	end
+	private
+	def  battle_score(winner,loser)
+		puts "#{winner.name} won!"		
+		if ((winner.is_a? Warrior)&&(winner.level<=loser.level))
+			winner.level_up(loser.level)
+		end
+	end
 end
