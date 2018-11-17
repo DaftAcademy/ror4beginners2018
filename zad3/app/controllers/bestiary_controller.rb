57fd1064b095339d1d052d9c52c9999d571539cf
Jakub Frąc <jakubfrac@MacBook-Pro-Jakub-2.local>
2018-11-17 21:30:49 +0100

    zad3

--- zad3/app/controllers/bestiary_controller.rb
+++ zad3/app/controllers/bestiary_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class BestiaryController < ApplicationController
+  attr_reader
   def index
 
   end
--- zad3/config/routes.rb
+++ zad3/config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'home/welcome'
   get 'bestiary/index'
   get 'bestiary/novigrad_monsters'
+  get '/', to: 'home#welcome', as: :home
+  get '/index', to: 'bestiary#index', as: :index
+  get '/novigrad', to: 'bestiary#novigrad_monsters', as: :novigrad
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
