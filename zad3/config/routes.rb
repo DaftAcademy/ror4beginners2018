0438add0e011f61767764ac8cd1d27ed9f48b819
Maxization <damian.opoka99@gmail.com>
2018-11-17 23:10:39 +0100

    zad3

--- zad3/config/routes.rb
+++ zad3/config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
 	get 'bestiary/index'
 	get '/', to: 'bestiary#index', as: :root
-  get 'bestiary/novigrad_monsters', as: :novigrad
+	get 'bestiary/novigrad_monsters', as: :novigrad
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
