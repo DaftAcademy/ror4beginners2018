cc7eea3a364890272d0c34da295bd9efd03d2f7f
Ariel Biernacki <arielbiernacki@gmail.com>
2018-11-18 19:10:32 +0100

    Add zad3 solution

--- zad3/app/controllers/bestiary_controller.rb
+++ zad3/app/controllers/bestiary_controller.rb
@@ -2,11 +2,11 @@
 
 class BestiaryController < ApplicationController
   def index
-
+    @monsters = monsters
   end
 
   def novigrad_monsters
-
+    @novigrad_monsters = monsters.select { |e| e[:location] == 'Novigrad'}
   end
 
   private
--- zad3/config/routes.rb
+++ zad3/config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'bestiary/index'
-  get 'bestiary/novigrad_monsters'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'bestiary#index', as: :root
+  get '/novigrad_monsters', to: 'bestiary#novigrad_monsters', as: :novigrad
 end
