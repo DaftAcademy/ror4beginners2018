6b7c0662a50d1267cfef33758b95101f7fb0cb60
Skarzynski <wiewior.zhr@gmail.com>
2018-11-19 20:44:16 +0100

    poprawki zad2

--- zad2/warriors.rb
+++ zad2/warriors.rb
@@ -1,53 +1,72 @@
 class Character
+  attr_accessor :name
+  attr_accessor :level
 
-attr_accessor :name
-attr_accessor :level
+  MIN_LEVEL = 1
+  MAX_LEVEL = 99
 
   def initialize(name:, level:)
-  	@level = level
-  	@name = name
+    @level = validate_level(level)
+    @name = name
   end
 
   def strength
-  	@level + [*1..12].sample
+    @level + [*1..12].sample
   end
 
   def card
-  	"#{@name} (lvl #{@level})"
+    "#{@name} (lvl #{@level})"
+  end
+
+  private
+
+  def validate_level(value)
+    [[MIN_LEVEL, value].max, MAX_LEVEL].min
   end
 end
 
 class Warrior < Character
-	def levelup(opponent_level)
-		@level += opponent_level-@level+1 if @level <=opponent_level
-		@level = 99 if @level >99
-	end
+  def levelup(opponent_level)
+    @level += opponent_level - @level + 1 if @level <= opponent_level
+  end
 end
 
 class Monster < Character
 end
 
-
 class BattleArena
   def initialize(first_character, second_character)
-  	@first_character = first_character
-  	@second_character = second_character
+    @first_character = first_character
+    @second_character = second_character
   end
 
   def battle!
-  	puts "#{@first_character.name} attacked #{@second_character.name} with #{strength1 = @first_character.strength} damage."
-  	puts "#{@second_character.name} attacked #{@first_character.name} with #{strength2 = @second_character.strength} damage."
+    strength1 = @first_character.strength
+    strength2 = @second_character.strength
 
-  	if strength1 > strength2 then
-  		puts "#{@first_character.name} won" 
-  		@first_character.levelup(@second_character.level) if @first_character.is_a? Warrior
-  	elsif strength2 > strength1 then
-  		puts "#{@second_character.name} won"
-  		@second_character.levelup(@first_character.level) if @second_character.is_a? Warrior
-  	else
-  		puts "Draw!"
-  	end
+    attack(@first_character, @second_character, strength1)
+    attack(@second_character, @first_character, strength2)
+
+    winner, loser = result(strength1, strength2)
+
+    if winner
+      winner.levelup(loser.level)
+      puts "#{winner.name} won!"
+    else
+      puts 'Draw!'
+    end
   end
 
-  			
+  private
+
+  def attack(attacker, defender, strength)
+    puts "#{attacker.name} attacked #{defender.name} with #{strength} strength"
+  end
+
+  def result(strength1, strength2)
+    case strength1 <=> strength2
+    when -1 then [@second_character, @first_character]
+    when 1 then [@first_character, @second_character]
+    end
+  end
 end
