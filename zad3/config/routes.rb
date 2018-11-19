933babdc9334bf253ea3e7245fed31278efcc80b
wmarosek <37310364+wmarosek@users.noreply.github.com>
2018-11-19 18:00:55 +0100

    zad3
    zad3 done!

--- zad3/config/routes.rb
+++ zad3/config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'bestiary/index'
-  get 'bestiary/novigrad_monsters'
+  root to: 'bestiary#index'
+  
+  get 'bestiary/index', to: 'bestiary#index', as: :bestiary_index
+  get 'bestiary/novigrad_monsters', to: 'bestiary#novigrad_monsters', as: :bestiary_novigrad_monsters
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
