1d43ead5f8a1b921353a3ae658a6e82f2798b338
ulatet <urszula.tetwejer@gmail.com>
2018-11-19 13:56:33 +0100

    Add existing file

---
+++ zad3/app/controllers/bestiary_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class BestiaryController < ApplicationController
+  def index
+    @monsters = monsters
+  end
+
+  def novigrad_monsters
+    @novigrad_monsters = monsters.select { |monster| monster[:location] == "Novigrad" }
+  end
+
+  private
+
+  def monsters
+    @monsters ||= begin
+      Faker::Config.random = Random.new(42)
+      (0..201).map do |n|
+        {
+          id: n,
+          name: Faker::Witcher.monster,
+          location: Faker::Witcher.location,
+          best_slayer: Faker::Witcher.character
+        }
+      end
+    end
+  end
+end
