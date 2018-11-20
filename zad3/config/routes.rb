7976fec74c58ac2ccc09fc3d8827eae7a674203a
Michal <ninnja@wp.pl>
2018-11-20 23:32:23 +0100

    Zadanie 3

--- zad3/config/routes.rb
+++ zad3/config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   get 'bestiary/index'
   get 'bestiary/novigrad_monsters'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
