3e1490c7a59d6cd5ace91c7481d72bac94b7c62a
ahawrylak <adrian.hawrylak@gmail.com>
2018-11-21 11:21:08 +0100

    feat: Add zad3 sample solution

--- zad3/app/controllers/bestiary_controller.rb
+++ zad3/app/controllers/bestiary_controller.rb
@@ -2,26 +2,30 @@
 
 class BestiaryController < ApplicationController
   def index
-
+    @monsters = monsters
   end
 
   def novigrad_monsters
-
+    @novigrad_monsters = monsters_from('Novigrad')
   end
 
   private
 
   def monsters
-    @monsters ||= begin
-      Faker::Config.random = Random.new(42)
-      (0..20).map do |n|
-        {
-          id: n,
-          name: Faker::Witcher.monster,
-          location: Faker::Witcher.location,
-          best_slayer: Faker::Witcher.character
-        }
-      end
+    Faker::Config.random = Random.new(42)
+    (0..20).map do |n|
+      {
+        id: n,
+        name: Faker::Witcher.monster,
+        location: Faker::Witcher.location,
+        best_slayer: Faker::Witcher.character
+      }
+    end
+  end
+
+  def monsters_from(location)
+    monsters.select do |monster|
+      monster[:location] == location
     end
   end
 end
