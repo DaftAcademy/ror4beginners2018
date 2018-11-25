d6f25978672182e302a017fdc624999ecf4b4fad
agrzelewska <agatagrzelewska@gmail.com>
2018-11-25 21:32:35 +0100

    Zad4 - first part

--- zad4/db/schema.rb
+++ zad4/db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_25_180157) do
+
+  create_table "players", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "power"
+    t.boolean "captain"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
