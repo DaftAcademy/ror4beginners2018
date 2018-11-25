b8903ec804a4a0ac6e1f9dd6e1d4b73a5a324e37
PrimaRoyale <alicja317@gmail.com>
2018-11-25 22:19:18 +0100

    Revert "zad3 completed"
    This reverts commit d71d26a7b0860e55a661f0716830564988b0e362.

--- zad4/app/services/football_player_manager.rb
+++ zad4/app/services/football_player_manager.rb
@@ -8,25 +8,14 @@ class FootballPlayerManager
   end
 
   def stronger_players
-    Player.where(power: (@player[:power]+1..99))
+    # TODO: Implement me pls
   end
 
   def set_power(value)
-    if (value > 99 || value <1)
-      p 'Power must be between 1 and 99'
-    else 
-      @player[:power] = value
-      @player.save
-      @player
-    end
+    # TODO: I need body
   end
 
   def copy_player
-    new_player = Player.new
-    new_player[:first_name] = @player[:first_name]
-    new_player[:last_name] = @player[:last_name]
-    new_player[:power] = @player[:power]
-    new_player.save
-    new_player
+    # TODO: I feel empty inside
   end
 end
--- zad4/db/schema.rb
+++ zad4/db/schema.rb
@@ -10,15 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_174619) do
-
-  create_table "players", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.integer "power"
-    t.boolean "captain", default: false, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
