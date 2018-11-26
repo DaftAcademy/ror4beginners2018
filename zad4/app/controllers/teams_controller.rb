8f131f9eeab8a46a7ebef14892314c2e44e15e57
Maxization <damian.opoka99@gmail.com>
2018-11-26 18:20:27 +0100

    zad4

--- zad4/app/controllers/teams_controller.rb
+++ zad4/app/controllers/teams_controller.rb
@@ -1,7 +1,7 @@
 class TeamsController < ApplicationController
   def index
-  	@captains=Player.where(captain: true).to_a
-  	@players=Player.where(captain: false).to_a
+    @captains=Player.where(captain: true).to_a
+    @players=Player.where(captain: false).to_a
     @teams=@players.count/@captains.count
     @random_captains=@captains.sample(@teams)
     @random_players=@players.sample(@teams*10)
