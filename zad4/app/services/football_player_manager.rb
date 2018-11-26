9b387d48b8a684f18d1b2736d00ef77d89e03b6f
zakpat <p.zak66@gmail.com>
2018-11-26 17:35:36 -0500

    zad4

--- zad4/app/services/football_player_manager.rb
+++ zad4/app/services/football_player_manager.rb
@@ -1,21 +1,27 @@
 # frozen_string_literal: true
 
-class FootballPlayerManager
+class FootballPlayerManager < ActiveRecord::Base
   attr_reader :player
 
   def initialize(player)
     @player = player
+    
   end
 
   def stronger_players
-    # TODO: Implement me pls
+    #@players = Players.all
+    #@players.each = {|link| p link}
+    Player.where('power > ?', player.power)
+    #puts @stronger
   end
 
   def set_power(value)
-    # TODO: I need body
+    player.update(power: value)
   end
 
   def copy_player
-    # TODO: I feel empty inside
+    copy = player.dup
+    copy.save
+    return copy
   end
 end
