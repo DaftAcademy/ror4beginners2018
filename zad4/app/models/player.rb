9810f10d75741735ceb8b445fe84f86c55b82835
Kacper Grabczak <kacpergrabczak@gmail.com>
2018-11-26 21:22:38 +0100

    Fixed tabs in player.rb

--- zad4/app/models/player.rb
+++ zad4/app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
-	validates :first_name, presence: true
-	validates :last_name, presence: true
-	validates :power, inclusion: 1..99
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :power, inclusion: 1..99
 end
