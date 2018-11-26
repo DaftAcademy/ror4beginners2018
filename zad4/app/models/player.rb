5f47d731c6d9cb76932270872a9442f52d183b81
ozel981 <ozel981@interia.pl>
2018-11-26 01:05:11 +0100

    zad4

--- zad4/app/models/player.rb
+++ zad4/app/models/player.rb
@@ -1,2 +1,5 @@
 class Player < ApplicationRecord
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :power, presence: true, numericality: {greather_than: 0, less_than: 100}
 end
--- zad4/app/services/football_player_manager.rb
+++ zad4/app/services/football_player_manager.rb
@@ -9,13 +9,18 @@ class FootballPlayerManager
 
   def stronger_players
     # TODO: Implement me pls
+    Player.where('power > ?', player[:power])
   end
 
   def set_power(value)
     # TODO: I need body
+    player.update(power: value)
   end
 
   def copy_player
     # TODO: I feel empty inside
+    copyed_player = player.dup
+    copyed_player.save
+    return copyed_player
   end
 end
