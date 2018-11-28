7ae1cb016fc36338137177fa56083a48bc57f37b
Szymon <szymon.chorobski@daftcode.pl>
2018-11-28 22:55:54 +0100

    Update track.rb

--- zad5/app/models/track.rb
+++ zad5/app/models/track.rb
@@ -1,5 +1,5 @@
 class Track < ApplicationRecord
   belongs_to :artist
   has_many :playlist_tracks, dependent: :destroy
-  has_many :playlists, through: :playlist_track
+  has_many :playlists, through: :playlist_tracks
 end
