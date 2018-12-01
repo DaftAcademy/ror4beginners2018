c048613344404aa1d02ad3ad38f3227c22ca6491
Skarzynski <wiewior.zhr@gmail.com>
2018-12-01 14:08:19 +0100

    zad 4 done

---
+++ zad4/app/controllers/teams_controller.rb
@@ -0,0 +1,10 @@
+class TeamsController < ApplicationController
+  def index
+    players = Player.order("random()").where(captain: false).first(20)
+    captains = Player.order("random()").where(captain: true).first(2)
+    @team1, @team2 = players.each_slice(10).to_a
+    @team1.unshift(captains.first)
+    @team2.unshift(captains.second)
+    @teams = [@team1, @team2]
+  end
+end
--- zad4/app/services/football_player_manager.rb
+++ zad4/app/services/football_player_manager.rb
@@ -8,14 +8,20 @@ class FootballPlayerManager
   end
 
   def stronger_players
+    Player.where('power > ?', player.power})
     # TODO: Implement me pls
   end
 
   def set_power(value)
+    player.power = value
+    Player.update(player.id, 'power' => value)
     # TODO: I need body
   end
 
   def copy_player
+    new_player = player.dup
+    new_player.save
+    new_player
     # TODO: I feel empty inside
   end
 end
