7fc207fb119476e0ec76522e42dc87018b5ecb56
OfftheCode <offthecode@gmail.com>
2018-12-02 18:14:52 +0100

    Basic routing and index | new | create methods works

--- zad5/config/routes.rb
+++ zad5/config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'tracks#index'
-
+  
+  resources :artists
   resources :tracks
   resources :playlists, only: %i[index show]
 end
