e2c75d42d0fceb271095f868ce6f6adc113d2df2
OfftheCode <offthecode@gmail.com>
2018-12-02 20:59:57 +0100

    Implemeted deleting

---
+++ zad5/app/controllers/artists_controller.rb
@@ -0,0 +1,35 @@
+class ArtistsController < ApplicationController
+    before_action :set_artist, only: %i[show update destroy edit]
+
+    def index
+        @artists = Artist.all
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.new(artist_params)
+        if @artist.save
+            redirect_to artists_url, notice: 'Artist has been saved!'
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+        @artist.destroy!
+        redirect_to artists_url, notice: 'Artist has been removed!'
+    end
+
+    private 
+
+    def set_artist
+        @artist = Artist.find(params[:id])
+    end
+
+    def artist_params
+        params.require(:artist).permit(:name)
+    end
+end
