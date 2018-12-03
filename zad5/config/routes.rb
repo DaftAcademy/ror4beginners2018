b4757c375e2d468ec50092965303309cdbbbfc52
Mateusz Zieba <mateusz.zieba@onet.pl>
2018-12-03 22:52:14 +0100

    zad4 solution

--- zad5/config/routes.rb
+++ zad5/config/routes.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'artists/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'tracks#index'
 
   resources :tracks
   resources :playlists, only: %i[index show]
+  resources :artists
 end
