75052a39be132223a2e942a448cf0598f99a1f69
Michal <ninnja@wp.pl>
2018-12-04 22:35:40 +0100

    Zadanie 5

--- zad5/app/models/track.rb
+++ zad5/app/models/track.rb
@@ -1,5 +1,6 @@
 class Track < ApplicationRecord
   belongs_to :artist
+  validates :title, presence: true
   has_many :playlist_tracks, dependent: :destroy
   has_many :playlists, through: :playlist_tracks
 end
