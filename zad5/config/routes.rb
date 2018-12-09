0089b9c6a0d177e8fe290c26001c487c7a5ca222
ulatet <34001430+ulatet@users.noreply.github.com>
2018-12-09 16:44:26 +0100

    Update routes.rb

--- zad5/config/routes.rb
+++ zad5/config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
 
-  get 'artists/index'
   root 'artists#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
