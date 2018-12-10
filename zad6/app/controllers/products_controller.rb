c015e2a8726ff794084459477ef0e45072a7fc1f
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2018-12-10 23:59:09 +0100

    create products endpoint

--- zad6/app/controllers/products_controller.rb
+++ zad6/app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def index
+    @customer = Customer.find(params[:customer_id])
+    @products = @customer.products.order(price: :desc).includes(:categories)
+  end
 end
