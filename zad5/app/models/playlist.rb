c5895421efab05535b696e6a724f9f832f099b9f
Jakub Horosz <kubahorosz95@gmail.com>
2018-12-11 14:19:16 +0100

    solved 5th task

--- zad5/app/models/playlist.rb
+++ zad5/app/models/playlist.rb
@@ -1,4 +1,4 @@
 class Playlist < ApplicationRecord
   has_many :playlist_tracks, dependent: :destroy
-  has_many :tracks, through: :playlist_tracks
+  has_many :tracks, through: :playlists_tracks
 end
